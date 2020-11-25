9ecc8311641f500df23e9c0a8750b5a75a5b606d
yongdol503 <yongdol503@gmail.com>
2020-11-25 14:40:43 +0900

    FIX

--- backend/router/main.js
+++ backend/router/main.js
@@ -1,12 +1,15 @@
 module.exports = function(app, fs)
 {
 
-     app.get('/',function(req,res){
-         res.render('index', {
-             title: "MY HOMEPAGE",
-             length: 5
-         })
-     });
+    app.get('/',function(req,res){
+        var sess = req.session;
+        res.render('index', {
+            title: "MY HOMEPAGE",
+            length: 5,
+            name: sess.name,
+            username: sess.username
+        })
+    });
 
     app.get('/list', function (req, res) {
        fs.readFile( __dirname + "/../data/user.json", 'utf8', function (err, data) {
@@ -58,7 +61,6 @@ module.exports = function(app, fs)
         })
     });
 
-
     app.put('/updateUser/:username', function(req, res){
 
         var result = {  };
@@ -87,7 +89,6 @@ module.exports = function(app, fs)
         })
     });
 
-
     app.delete('/deleteUser/:username', function(req, res){
         var result = { };
         //LOAD DATA
@@ -114,6 +115,52 @@ module.exports = function(app, fs)
             })
         })
 
+    })
+
+    app.get('/login/:username/:password', function(req, res){
+        var sess;
+        sess = req.session;
+
+        fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data){
+            var users = JSON.parse(data);
+            var username = req.params.username;
+            var password = req.params.password;
+            var result = {};
+            if(!users[username]){
+                // USERNAME NOT FOUND
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+
+            if(users[username]["password"] == password){
+                result["success"] = 1;
+                sess.username = username;
+                sess.name = users[username]["name"];
+                res.json(result);
+
+            }else{
+                result["success"] = 0;
+                result["error"] = "incorrect";
+                res.json(result);
+            }
+        })
+    });
+
+    app.get('/logout', function(req, res){
+        sess = req.session;
+        if(sess.username){
+            req.session.destroy(function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.redirect('/');
+                }
+            })
+        }else{
+            res.redirect('/');
+        }
     })
 
 }
